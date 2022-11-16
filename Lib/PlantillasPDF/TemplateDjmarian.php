af5d3708ab8850cc74d4d0db9268fe65a3dea5c6
Ferran Llop <contacto@ferranllop.com>
2022-11-16 20:18:13 +0100

    Fix lines amount header and rearrange total taxes columns

--- Lib/PlantillasPDF/TemplateDjmarian.php
+++ Lib/PlantillasPDF/TemplateDjmarian.php
@@ -17,112 +17,23 @@ class TemplateDjmarian extends \FacturaScripts\Plugins\PlantillasPDF\Lib\Plantil
 
     /**
      * 
-     * @return string
-     */
-    protected function headerLeft(): string
-    {
-        $contactData = [];
-        foreach (['telefono1', 'telefono2', 'email', 'web'] as $field) {
-            if ($this->empresa->{$field}) {
-                $contactData[] = $this->empresa->{$field};
-            }
-        }
-
-        $title = $this->showHeaderTitle ? '<h1 class="title">' . $this->get('headertitle') . '</h1>' . $this->spacer() : '';
-        return '<table class="table-big">'
-            . '<tr>'
-            . '<td valign="top"><img src="' . $this->logoPath . '" height="' . $this->get('logosize') . '"/></td>'
-            . '<td align="right" valign="top">'
-            . $title
-            . '<p><b>' . $this->empresa->nombre . '</b>'
-            . '<br/>' . $this->empresa->tipoidfiscal . ': ' . $this->empresa->cifnif
-            . '<br/>' . $this->combineAddress($this->empresa) . '</p>'
-            . $this->spacer()
-            . '<p>' . \implode(' · ', $contactData) . '</p>'
-            . '</td>'
-            . '</tr>'
-            . '</table>';
-    }
-    /**
-     * 
      * @param string $txt
      *
      * @return string
      */
     protected function getInvoiceLineFieldTitle(string $txt): string
     {
-        $codes = [
-            'cantidad' => 'quantity-abb',
-            'descripcion' => 'description',
-            'dtopor' => 'dto',
-            'dtopor2' => 'dto-2',
-            'iva' => 'tax-abb',
-            'pvpunitario' => 'price',
-            'pvptotal' => 'amount',
-            'recargo' => 're',
-            'referencia' => 'reference'
-        ];
-
-        return isset($codes[$txt]) ? $this->toolBox()->i18n()->trans($codes[$txt]) : $this->toolBox()->i18n()->trans($txt);
+        return parent::getInvoiceLineFieldTitle($this->fixTotalLinesHeading($txt));
     }
 
     /**
-     * 
-     * @param BusinessDocument $model
-     *
-     * @return string
+     * Replace the heading from pvptotal that prints 'Net' heading,
+     * which is incorrect, to amount that prints 'Amount'
      */
-    protected function getInvoiceHeaderResume($model): string
+    private function fixTotalLinesHeading(string $txt): string
     {
-        $i18n = $this->toolBox()->i18n();
-
-        $extra1 = '';
-        if ($this->get('logoalign') === 'full-size') {
-            $title = empty($this->format->titulo) ? $i18n->trans($model->modelClassName() . '-min') : $this->format->titulo;
-            $extra1 .= '<tr>'
-                . '<td><b>' . $title . '</b>:</td>'
-                . '<td>' . $model->codigo . '</td>'
-                . '</tr>';
-        }
-
-        /// rectified invoice?
-        if (isset($model->codigorect) && !empty($model->codigorect)) {
-            $extra1 .= '<tr>'
-                . '<td><b>' . $i18n->trans('original') . '</b>:</td>'
-                . '<td>' . $model->codigorect . '</td>'
-                . '</tr>';
-        }
-
-        /// number2?
-        $extra2 = '';
-        if (isset($model->numero2) && !empty($model->numero2) && (bool) $this->get('shownumero2')) {
-            $extra2 .= '<tr>'
-                . '<td><b>' . $i18n->trans('number2') . '</b>:</td>'
-                . '<td>' . $model->numero2 . '</td>'
-                . '</tr>';
-        }
-
-        /// cif/nif?
-        $extra3 = empty($model->cifnif) ? '' : '<tr>'
-            . '<td><b>' . $model->getSubject()->tipoidfiscal . '</b>:</td>'
-            . '<td>' . $model->cifnif . '</td>'
-            . '</tr>';
-
-        $size = empty($extra2) ? 170 : 200;
-        return '<td valign="top" width="' . $size . '">'
-            . '<table class="table-big">'
-            . $extra1
-            . '<tr>'
-            . '<td><b>' . $i18n->trans('date') . '</b>:</td>'
-            . '<td>' . $model->fecha . '</td>'
-            . '</tr>'
-            . $extra2
-            . $extra3
-            . $this->getInvoiceHeaderResumePhones($model->getSubject())
-            . '</table>'
-            . '</td>';
+        return $txt == 'pvptotal' ? 'amount' : $txt;
     }
-
 
     /**
      * 
@@ -133,6 +44,15 @@ class TemplateDjmarian extends \FacturaScripts\Plugins\PlantillasPDF\Lib\Plantil
      * @return string
      */
     protected function getInvoiceTaxes($model, $lines, $class = 'table-big'): string
+    {
+        return $this->fixInvoiceTaxesColumns($model, $lines, $class);
+    }
+
+    /*
+     * Remove 'percentage' column and move 'tax' column 
+     * to come after 'taxbase' column 
+     */
+    private function fixInvoiceTaxesColumns($model, $lines, $class): string
     {
         $rows = $this->getTaxesRows($model, $lines);
         if (empty($model->totaliva)) {
@@ -147,6 +67,7 @@ class TemplateDjmarian extends \FacturaScripts\Plugins\PlantillasPDF\Lib\Plantil
         foreach ($rows as $row) {
             $trs .= '<tr>'
                 . '<td align="center">' . $coins->format($row['taxbase']) . '</td>'
+                . '<td align="center">' . $row['tax'] . '</td>'
                 . '<td align="center">' . $coins->format($row['taxamount']) . '</td>';
 
             if (empty($model->totalrecargo)) {
@@ -164,7 +85,8 @@ class TemplateDjmarian extends \FacturaScripts\Plugins\PlantillasPDF\Lib\Plantil
                 . '<thead>'
                 . '<tr>'
                 . '<th align="center">' . $i18n->trans('tax-base') . '</th>'
-                . '<th align="center">' . $row['tax'] . '</th>'
+                . '<th align="center">' . $i18n->trans('tax') . '</th>'
+                . '<th align="center">' . $i18n->trans('amount') . '</th>'
                 . '</tr>'
                 . '</thead>'
                 . $trs
@@ -173,9 +95,9 @@ class TemplateDjmarian extends \FacturaScripts\Plugins\PlantillasPDF\Lib\Plantil
 
         return '<table class="' . $class . '">'
             . '<tr>'
-            . '<th align="center">' . $i18n->trans('tax') . '</th>'
             . '<th align="center">' . $i18n->trans('tax-base') . '</th>'
-            . '<th align="center">' . $row['tax'] . '</th>'
+            . '<th align="center">' . $i18n->trans('tax') . '</th>'
+            . '<th align="center">' . $i18n->trans('amount') . '</th>'
             . '<th align="center">' . $i18n->trans('re') . '</th>'
             . '<th align="right">' . $i18n->trans('amount') . '</th>'
             . '</tr>'
